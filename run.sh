76393fe7764d74db78f231525c5251ec87a8dd09
sidkothiyal <sid.kothiyal27@gmail.com>
2017-06-04 03:19:18 +0530

    GUI starting

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
-sudo apt-get install python2.7-dev python3.5-dev python-pip python-pip3
-sudo apt-get install build-essential freeglut3-dev
+sudo apt-get install python2.7-dev python-pip 
+sudo apt-get install build-essential freeglut3-dev python-qt4
 sudo pip install pyopengl
+python main.py
 python graph.py
+
