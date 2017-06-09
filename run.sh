467bdc0febbe1ca3fbf00c54572d12ac935d4299
sidkothiyal <sid.kothiyal27@gmail.com>
2017-06-09 17:36:48 +0530

    run.sh fixed

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 sudo apt-get install python2.7-dev python-pip 
-sudo apt-get install build-essential freeglut3-dev python-qt4
+sudo apt-get install libxext-dev python-qt4 qt4-dev-tools build-essential
+sudo apt-get install freeglut3-dev 
 sudo pip install pyopengl
 python main.py
 python graph.py
