a8180a6edf69797776c0db1c359c29a70be047c8
sidkothiyal <sid.kothiyal27@gmail.com>
2017-06-11 05:03:19 +0530

    tokenizer improvements

--- tokenize.py
+++ tokenize.py
@@ -1,4 +1,6 @@
-
+symbols = ['+', '-', '*', '/', '{', '}', '[',']', '^', '=']
+greek = [u'\u03B1', u'\u03B2', u'\u03B3', u'\u03C0']
+	
 def removeSpaces(eqn):
 	cleanEqn = ''
 	x = 0
@@ -10,40 +12,70 @@ def removeSpaces(eqn):
 		x += 1		
 	return cleanEqn
 	
-def clean(eqn):
-	cleanEqn = removeSpaces(eqn)
+def getTerms(eqn):
 	x = 0
 	terms = []
-	symbols = ['+', '-', '*', '/', '{', '}', '[',']', '^', '=']
-	greek = [u'\u03B1', u'\u03B2', u'\u03B3', u'\u03C0']
-	while x < len(cleanEqn):
-		if (cleanEqn[x] >= 'a' and cleanEqn[x] <= 'z') or (cleanEqn[x] >= 'A' and cleanEqn[x] <= 'Z'):
-				terms.append(cleanEqn[x])
+	while x < len(eqn):
+		if (eqn[x] >= 'a' and eqn[x] <= 'z') or (eqn[x] >= 'A' and eqn[x] <= 'Z') or eqn[x] in greek:
+				terms.append(eqn[x])
 				x += 1	
-		elif cleanEqn[x] == '\\':
+		elif eqn[x] == '\\':
 			buf = '\\'
 			x += 1
-			while (cleanEqn[x] >= a and cleanEqn[x] <= z) or (cleanEqn[x] >= A and cleanEqn[x] <= Z):
-				buf += cleanEqn[x]
+			while (eqn[x] >= 'a' and eqn[x] <= 'z') or (eqn[x] >= 'A' and eqn[x] <= 'Z'):
+				buf += eqn[x]
 			  	x +=1
 			terms.append(buf)
-		elif cleanEqn[x] > '0' and cleanEqn[x] < '9':
-			buf = cleanEqn[x]
+		elif eqn[x] > '0' and eqn[x] < '9':
+			buf = eqn[x]
 			x += 1
-			while cleanEqn[x] > '0' and cleanEqn[x] < '9':
-				buf += cleanEqn[x]
+			while eqn[x] > '0' and eqn[x] < '9':
+				buf += eqn[x]
 				x += 1
 			terms.append(buf)
-		elif cleanEqn[x] in symbols:				
-			terms.append(cleanEqn[x])
+		elif eqn[x] in symbols:				
+			terms.append(eqn[x])
 			x += 1
 		else:
-			x += 1	
-		print x	
-	print terms
+			x += 1
+	return terms
 
+def getVariable(terms):
+	variable = {}
+	variable["type"] = "variable"
+	value = []
+	coefficient = 1
+	power = []
+	value.append(terms[x])
+	power.append(1)
+	while terms[x] != '+' and terms[x] != '-' and terms[x] != '*' and terms[x] != '*' and terms[x] != '=':
+		if (terms[x][0] >= 'a' and terms[x][0] <= 'z') or (terms[x][0] >= 'A' and terms[x][0] <= 'Z') or terms[x][0] in greek:
+			value.append(terms[x])
+			power.append(1) 	
+	variable["value"] = value
+	variable["coefficient"] = coefficient
+	
 
-def tokenizer(eqn="    x     +     y   =    2   "):
+def getToken(terms):
+	tokens = []
+	x = 0
+	while x < len(terms):
+		if (terms[x][0] >= 'a' and terms[x][0] <= 'z') or (terms[x][0] >= 'A' and terms[x][0] <= 'Z') or terms[x][0] in greek:
+			varTerms = []
+			while terms[x] != '+' and terms[x] != '-' and terms[x] != '*' and terms[x] != '*' and terms[x] != '=':
+				varTerms.append(terms[x])
+				x += 1
+			variable = getVariable(varTerms)
+			tokens.append(variable)
+		x += 1	
+	print tokens		  
+
+				
+def clean(eqn):
+	cleanEqn = removeSpaces(eqn)
+	terms = getTerms(cleanEqn)
+	
+def tokenizer(eqn="    x     +     y^22    =    22   "):
 	clean(eqn)
 
 if __name__ == "__main__":
