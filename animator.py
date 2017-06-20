1ef5d7f0e99f89da34d1c84cdcf0f9c8e912acf7
sidkothiyal <sid.kothiyal27@gmail.com>
2017-06-21 01:17:03 +0530

    complex variable animator

--- animator.py
+++ animator.py
@@ -8,7 +8,8 @@ from OpenGL.GLUT import *
 
 import FTGL
 
-fonts = []
+fontStyle = "/usr/share/fonts/truetype/ubuntu-font-family/UbuntuMono-R.ttf"  
+font = FTGL.BitmapFont(fontStyle)
 width = 600
 height = 600
 symbols = ['+', '-', '*', '/', '{', '}', '[',']', '^', '=']
@@ -18,7 +19,7 @@ inputLaTeX = ['\\times', '\\div', '\\alpha', '\\beta', '\\gamma', '\\pi', '+', '
 inputGreek = ['*', '/', u'\u03B1', u'\u03B2', u'\u03B3', u'\u03C0', '+', '-', '=', '^', 'sqrt']
 
 
-def isVariable(term):
+def is_variable(term):
     if term in greek: 
         return True
     elif (term[0] >= 'a' and term[0] <= 'z') or (term[0] >= 'A' and term[0] <= 'Z'):
@@ -29,7 +30,7 @@ def isVariable(term):
             x += 1
         return True
 
-def isNumber(term):
+def is_number(term):
     x = 0
     while x < len(term):
         if term[x] < '0' or term[x] > '9':
@@ -37,6 +38,53 @@ def isNumber(term):
         x += 1  
     return True
 
+
+def render_variable(x, y, term, level=1, fontSize=24):
+    font.FaceSize(24)
+    if term["coefficient"] == 1:
+        pass
+    elif term["coefficient"] == -1:
+        font.Render(str('-'))
+        x += 10
+    else:
+        font.Render(str(term["coefficient"]))
+        x += 10
+    if len(term["value"])> 0:
+        for j, val in enumerate(term["value"]):
+            if type(val) == dict:
+                pass
+            elif is_variable(str(val)) or is_number(str(val)):
+                glRasterPos(x, y)
+                font.FaceSize(fontSize)
+                if val in greek:
+                    font.Render(str(val.encode('utf-8')))
+                else:
+                    font.Render(str(val)) 
+            x += 10
+            if type(term["power"][j]) == dict:
+                pass
+            elif is_variable(str(term["power"][j])):
+                glRasterPos(x, y + 10)
+                font.FaceSize(2  * fontSize/3)
+                if term["power"][j] in greek:
+                    font.Render(str(term["power"][j].encode('utf-8')))
+                else:
+                    font.Render(str(term["power"][j]))
+                x += 20   
+            elif is_number(str(term["power"][j])):
+                if term["power"][j] == 1:
+                    x += 10
+                    pass
+                else:
+                    glRasterPos(x, y + 10)
+                    font.FaceSize(2  * fontSize/3)
+                    if term["power"][j] in greek:
+                        font.Render(str(term["power"][j].encode('utf-8')))
+                    else:
+                        font.Render(str(term["power"][j]))
+                    x += 20           
+                    
+    return x, y
 
 def do_ortho():
     
@@ -67,12 +115,11 @@ def draw_scene():
     string.append([{'coefficient': 1, 'type': 'variable', 'power': [1], 'value': ['x']}, {'type': 'binary', 'value': '+'}, {'coefficient': 1, 'type': 'variable', 'power': [1], 'value': ['y']}, {'type': 'binary', 'value': '='}, {'coefficient': 1, 'type': 'variable', 'power': [{'coefficient': 1, 'type': 'variable', 'power': [1, 2], 'value': ['x', 'y']}], 'value': [2]}])
     
     glColor3f(1.0, 1.0, 1.0)
-    font = fonts[0]
     x, y = 0, 0
     i = 0
     #while  True:
     i += 1
-    render_equation(x, y, string[0])
+    render_equation(x, y, string[1])
     
     glutSwapBuffers()
     time.sleep(10)
@@ -107,20 +154,17 @@ def render_equation(x, y, string):
 
     for i, term in enumerate(string):
         if term["type"] == "variable":
-            if len(term["value"])> 0:
-                for j, val in enumerate(term["value"]):
-                    if isVariable(val) or isNumber(str(val)):
-                        glRasterPos(x, y)
-                        x += 20
-                        font.Render(str(val))
+            x, y = render_variable(x, y, term)
         elif term["type"] == "constant":
                 glRasterPos(x, y)
+                font.FaceSize(24, 72)
                 x += 20
                 font.Render(str(term["value"]))
         elif term["type"] == "binary":
             if len(term["value"])> 0:
                 glRasterPos(x, y)
                 x += 20
+                font.FaceSize(24, 72)
                 font.Render(term["value"])
 
 
@@ -143,23 +187,7 @@ if __name__ == '__main__':
     glutInitDisplayMode(GLUT_RGBA|GLUT_DOUBLE)
     glutCreateWindow("Equation")
     glClearColor(0.0, 0.0, 0.0, 0.0)
-    f ="/usr/share/fonts/truetype/ubuntu-font-family/UbuntuMono-R.ttf"
-    #print sys.argv[1]
-    try:
-
-        fonts = [
-            #FTGL.OutlineFont(f),
-            #FTGL.PolygonFont(f),
-            #FTGL.TextureFont(f),
-            FTGL.BitmapFont(f),
-            #FTGL.PixmapFont(f),
-            ]
-        for font in fonts:
-            font.FaceSize(24, 72)
-    except Exception as e:
-	print e
-        print "usage:", sys.argv[0], "font_filename.ttf"
-        sys.exit(0)
+    font.FaceSize(24, 72)
 
     glutDisplayFunc(on_display)
     glutReshapeFunc(on_reshape)
