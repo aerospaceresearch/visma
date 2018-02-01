7f466ac1387eb93ca230c6fe109a20eebbfdcfd2
sidkothiyal <sid.kothiyal27@gmail.com>
2018-02-01 22:46:28 +0530

    type class  and bug fixes

--- types.py
+++ types.py
@@ -23,15 +23,48 @@ class Constant(BaseTypes):
 		if scope != None:
 			self.scope = scope
 
+	def remove_ones(self):
+		#remove random ones
+		pass
+
 	def __str__(self):
 		pass
 
 	def __repr__(self):
 		print "Constant"
-		if self.coefficient == -1:
+		if isinstance(self.coefficient, Constant):
+			self.coefficient.__repr__()
+			print "check coeff in " + self.coefficient + self.value + self.power
+		elif self.coefficient == -1:
 			print('-', end='')
+		elif self.coefficient != 0:
+			print(self.coefficient, end='')
+		else:
+			pass
+			#delete the constant
+
 		if isinstance(self.value, list):
-			
+			for i, val in enumerate(self.value):
+				if isinstance(val, Constant):
+					val.__repr__()
+					print "check " + i + " in " + self.value + self.power
+				elif val != 1 or val != 0:
+					print(val, end='')
+					if isinstance(self.power[i], Constant):
+						self.power[i].__repr__()
+					elif self.power[i] == 0:
+						self.power[i] = 1
+						self.value[i] = 1	
+					elif self.power[i] != 1:
+						print('^' +self.power, end=' ')
+					else:
+						print(' ')
+				elif val == 0:
+					pass
+					#delete const
+
+
+
 
 class Variable(BaseTypes):
 	"""docstring for Variable"""
@@ -49,6 +82,28 @@ class Variable(BaseTypes):
 			self.coefficient = coefficient
 		if scope != None:
 			self.scope = scope
+
+	def __str__(self):
+		pass
+
+	def __repr__(self):
+		print "Variable"
+		if self.coefficient == -1:
+			print('-', end='')
+		elif self.coefficient != 0:
+			print(self.coefficient, end='')
+		else:
+			pass
+			#delete the variable
+
+		if isinstance(self.value, list):
+			for i, val in enumerate(self.value):
+				if val != 1:
+					print(val, end='')
+					if self.power[i] != 1:
+						print('^' +self.power, end=' ')
+					else:
+						print(' ')
 			
 
 class Operations(BaseTypes):
