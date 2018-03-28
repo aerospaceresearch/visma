c20490bfc67490be7b8cba858a077b18d0520033
WiCK3D-POiSON <wick3d.poison@gmail.com>
2018-03-28 19:48:17 +0530

    Added calculus.py Baby steps towards differentiation

---
+++ calculus.py
@@ -0,0 +1,45 @@
+###################
+# Differentiation #
+###################
+
+# following is for differentiating functions within functions like func1(func2(func3))
+# TODO: Find workaround for differentiating func1(func2+func3)
+
+import functions
+
+def differentiation(func):
+
+    funclist = []
+
+    while(func.__class__ != Variable):
+
+        func.coefficient *= func.power
+        func.power -= 1
+        if(func.power == 0):
+            const = func
+            const.value = const.coefficient
+            const.differentiate()
+            funclist.append(const)
+        else:
+            funclist.append(func)
+
+        func.differentiate()
+        funclist.append(func)
+
+# TODO: Send each of these steps to animator
+
+        '''
+        animator code
+        '''
+
+        func = func.operand
+
+        if(func.__class__ == Constant):
+            func.value = 0
+            func.coefficient = 1
+            func.power = 1
+            funclist = [func]
+            break
+
+# The differentiated function is the product of all functions in funclist
+return funclist[]
