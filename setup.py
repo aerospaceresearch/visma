e64c803ac39063bb06ea190c8769ca041e0de072
WiCK3D-POiSON <wick3d.poison@gmail.com>
2018-08-11 02:22:15 +0530

    Modified setup.py Added pack script Added symbolic link to visma

--- setup.py
+++ setup.py
@@ -1,12 +1,23 @@
 import setuptools
+import pip
+try:
+    from pip._internal.utils.misc import get_installed_distributions
+except ImportError:  # pip < 10
+    from pip import get_installed_distributions
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
+packages = [package.project_name for package in get_installed_distributions()]
+
+requirements = ['PyQt5', 'matplotlib', 'numpy', 'coverage', 'pytest']
+for requirement in requirements:
+    if requirement not in packages:
+        pip.main(['install', '--user', requirement])
 
 setuptools.setup(
     name="VISualMAth",
     description="visma - VISual MAth : A math equation solver and visualizer",
-    version="0.2.1",
+    version="0.2.0",
     author="Siddharth Kothiyal, Shantanu Mishra",
     author_email="sid.kothiyal27@gmail.com, 8hantanu@gmail.com",
     long_description=long_description,
@@ -19,6 +30,7 @@ setuptools.setup(
         'Chat': 'https://gitter.im/aerospaceresearch/visma'
     },
     packages=setuptools.find_packages(),
+    scripts=['bin/visma'],
     classifiers=(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
@@ -26,5 +38,6 @@ setuptools.setup(
         "Topic :: Scientific/Engineering",
         "Topic :: Scientific/Engineering :: Mathematics",
         "Topic :: Scientific/Engineering :: Visualization"
-    )
+    ),
+    python_requires='>=3'
 )
