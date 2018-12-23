e369cef2d80621184ce9e40cdb69bcac46ee40a7
WiCK3D-POiSON <wick3d.poison@gmail.com>
2018-12-24 00:17:40 +0530

    Fixed multiplication

--- visma/simplify/muldiv.py
+++ visma/simplify/muldiv.py
@@ -64,16 +64,25 @@ def expressionMultiplication(variables, tokens):
                             for k, var2 in enumerate(tokens[i - 1].value):
                                 tokens[i - 1].coefficient *= tokens[i + 1].coefficient
                                 if var == var2:
-                                    if tokens[i + 1].power[j] == tokens[i - 1].power[k]:
-                                        if isNumber(tokens[i + 1].power[j]) and isNumber(tokens[i - 1].power[k]):
-                                            tokens[i - 1].power[k] += tokens[i + 1].power[j]
-                                            found = True
-                                            break
+                                    if isNumber(tokens[i + 1].power[j]) and isNumber(tokens[i - 1].power[k]):
+                                        tokens[i - 1].power[k] += tokens[i + 1].power[j]
+                                        if tokens[i - 1].power[k] == 0:
+                                            del tokens[i - 1].power[k]
+                                            del tokens[i - 1].value[k]
+                                        found = True
+                                        break
                             if not found:
                                 tokens[i - 1].value.append(tokens[i + 1].value[j])
                                 tokens[i - 1].power.append(tokens[i + 1].power[j])
-                        removeScopes.append(tokens[i].scope)
-                        removeScopes.append(tokens[i + 1].scope)
+
+                            if len(tokens[i - 1].value) == 0:
+                                constant = Constant()
+                                constant.scope = tokens[i - 1].scope
+                                constant.power = 1
+                                constant.value = tokens[i - 1].coefficient
+                                tokens[i - 1] = constant
+                            removeScopes.append(tokens[i].scope)
+                            removeScopes.append(tokens[i + 1].scope)
                         return variables, tokens, removeScopes, comments
 
                     elif (isinstance(tokens[i + 1], Variable) and isinstance(tokens[i - 1], Constant)):
