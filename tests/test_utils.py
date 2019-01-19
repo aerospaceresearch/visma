67fe1806f73b4e3aa94e64e6bebe34964517a607
Alpesh Jamgade <alpeshjamgade21@gmail.com>
2019-01-19 11:49:21 +0530

    Update test_utils.py

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -26,4 +26,5 @@ def test_syntheticDivision():
     assert syntheticDivision([1, 2, 1], -1) == ([1.0, 1.0], 0)
     # (x^2 + 2x + 1)/(x+1)
     assert syntheticDivision([3, 2, 1, 3], 2) == ([3.0, 8.0, 17.0], 37)
-    # (3x^2 + 2x + x + 3)/(x-2
+    # (3x^2 + 2x + x + 3)/(x-2)
+    
