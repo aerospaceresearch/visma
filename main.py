6f4ee4b7513430ce3bb71a77711575e826793a84
Pratyush Harsh <pratyush9000@gmail.com>
2019-03-09 21:38:10 +0530

    Added clear command to clear the screen and improved CLI
    Improved the appearance and added clear screen command

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import os
 from visma.gui.window import initGUI
 from visma.gui.cli import commandExec
 from visma.gui import logger
+from subprocess import call
+from pyfiglet import Figlet
 
 
 def init():
@@ -9,20 +11,34 @@ def init():
     logger.setLevel(10)
     logger.setLogName('main')
     logger.info('Initialising VisMa...(currently in CLI mode)')
+    ClearScr()
     cin = input('>>> ')
     while(cin != 'exit'):
         if cin == 'gui':
             initGUI()
+        if cin =='clear':
+            ClearScr()
         else:
             try:
                 commandExec(cin)
             except Exception:
                 logger.error("Invalid Expression: %s ", cin)
                 print("Invalid Expression")
+                pass
         cin = input('>>> ')
     if (cin == 'exit'):
         logger.info('Exiting VisMa...')
-
+def ClearScr():
+    try:
+        call('clear')#in case of linux/unix/mac 
+    except:
+        call('cls')#in case of windows
+    print("")
+    print("__"*100);
+    f = Figlet(font='slant')
+    print(f.renderText('VISual MAth'))
+    print("")
+    print("__"*100);
 
 if __name__ == '__main__':
     init()
