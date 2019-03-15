5b53cbbc28a5ceafb67ebc08934815f5b0a9037a
Alii-Khaled <id.ali1230@gmail.com>
2019-03-15 13:22:19 +0200

    Added UserInterface for CLI.

--- main.py
+++ main.py
@@ -9,8 +9,26 @@ def init():
     logger.setLevel(10)
     logger.setLogName('main')
     logger.info('Initialising VisMa...(currently in CLI mode)')
-    cin = input('>>> ')
-    while(cin != 'exit'):
+    userInterface = "_______________________________________________________________________________________________\n"\
+                    "| gui  ->> opens Visma in GUI mode.                                                           |\n"\
+                    "| exit ->> Closes the program.                                                                |\n"\
+                    "|---------------------------------------------------------------------------------------------|\n"\
+                    "| simplify(equation or expression) ->> Simplifies the given equation.                         |\n"\
+                    "| addition(equation or expression) ->> Adds the elements used.                                |\n"\
+                    "| subtraction(equation or expression) ->> Subtracts the elements used.                        |\n"\
+                    "| multiplication(equation or expression)  ->> Multiplies the elements used.                   |\n"\
+                    "| division(equation or expression)  ->> Divides the elements used.                            |\n"\
+                    "|---------------------------------------------------------------------------------------------|\n"\
+                    "| factorize(expression)  ->> Factorizes the expression.                                       |\n"\
+                    "| find-roots(equation)  ->> Solves the quadratic equation for the variable in the equation.   |\n"\
+                    "| solve(equation , variable)  ->> Solves the equation for the given variable.                 |\n"\
+                    "|---------------------------------------------------------------------------------------------|\n"\
+                    "| integrate(expression , variable)  ->> Integrates the expression by the given variable.      |\n"\
+                    "| differentiate(expression , variable)->> Differentiates the expression by the given variable.|\n"\
+                    "|_____________________________________________________________________________________________|\n"\
+                    ">>> "
+    cin = input(userInterface)
+    while cin != 'exit':
         if cin == 'gui':
             initGUI()
         else:
@@ -20,7 +38,7 @@ def init():
                 logger.error("Invalid Expression: %s ", cin)
                 print("Invalid Expression")
         cin = input('>>> ')
-    if (cin == 'exit'):
+    if cin == 'exit':
         logger.info('Exiting VisMa...')
 
 
