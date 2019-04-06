24b0ea9a75f8673e36ee5502465cfdbbac985625
Alii-Khaled <id.ali1230@gmail.com>
2019-04-06 18:05:28 +0200

    remove unwanted UI message,

--- main.py
+++ main.py
@@ -4,25 +4,6 @@ from visma.gui.cli import commandExec
 from visma.gui.window import initGUI
 from visma.gui import logger
 
-userInterface = "___________________________________________________________________________________________________\n"\
-                "| gui  ->> opens Visma in GUI mode.                                                               |\n"\
-                "| exit ->> Closes the program.                                                                    |\n"\
-                "|-------------------------------------------------------------------------------------------------|\n"\
-                "| simplify( equation or expression ) ->> Simplifies the given equation.                           |\n"\
-                "| addition( equation or expression ) ->> Adds the elements used.                                  |\n"\
-                "| subtraction( equation or expression ) ->> Subtracts the elements used.                          |\n"\
-                "| multiplication( equation or expression )  ->> Multiplies the elements used.                     |\n"\
-                "| division( equation or expression )  ->> Divides the elements used.                              |\n"\
-                "|-------------------------------------------------------------------------------------------------|\n"\
-                "| factorize( expression )  ->> Factorizes the expression.                                         |\n"\
-                "| find-roots( equation )  ->> Solves the quadratic equation for the variable in the equation.     |\n"\
-                "| solve( equation , variable )  ->> Solves the equation for the given variable.                   |\n"\
-                "|-------------------------------------------------------------------------------------------------|\n"\
-                "| integrate( expression , variable )  ->> Integrates the expression by the given variable.        |\n"\
-                "| differentiate( expression , variable )->> Differentiates the expression by the given variable.  |\n"\
-                "|_________________________________________________________________________________________________|\n"\
-                ">>> "
-
 
 def init():
     open(os.path.abspath("log.txt"), "w").close()
