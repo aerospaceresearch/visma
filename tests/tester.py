2f949a492205f0c5f18c607ad904e9e18584155a
mayankDhiman <mdhiman536@gmail.com>
2019-04-20 03:14:29 +0530

    modified tester to incorporate simultaneous eqns

--- tests/tester.py
+++ tests/tester.py
@@ -5,6 +5,14 @@ from visma.io.checks import checkTypes
 
 
 def quickTest(inp, operation, wrtVar=None):
+    if ';' in inp:
+        afterSplit = inp.split(';')
+        # Multiple equations provided by user.
+        eqStr1 = afterSplit[0]
+        eqStr2 = afterSplit[1]
+        eqStr3 = afterSplit[2]
+        resultStr = operation(eqStr1, eqStr2, eqStr3, wrtVar)
+        return removeSpaces(resultStr)
     lhs, rhs = getLHSandRHS(tokenizer(inp))
     _, inpType = checkTypes(lhs, rhs)
     if inpType == "equation":
