43ca0d088ba6a2c4780edcbb0a2cb21977426775
Ayush <ayush.mittal9398@gmail.com>
2019-04-26 02:30:56 +0530

    tests added

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -1,5 +1,6 @@
 from visma.functions.constant import Constant
 from visma.functions.variable import Variable
+from tests.tester import getTokens
 
 
 ######################
@@ -39,3 +40,21 @@ def test_Variable():
     assert isinstance(variable2, Expression)
     assert variable2.__str__() == '{3log{x}}'
     '''
+
+
+#######################
+# functions.structure #
+#######################
+
+
+def test_reduce():
+
+    expr = getTokens("(2x+x)")
+    assert str(expr) == "3.0{x}"
+    assert expr.type == "Variable"
+
+    expr = getTokens("(3x+4*(x+(2x))+2y)")
+    assert str(expr) == "{(15.0{x}+2.0{y})}"
+
+    expr = getTokens("((2x+4x)+(2y+4y))")
+    assert str(expr) == "{(6.0{x}+6.0{y})}"
