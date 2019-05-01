8552313ebe306334376c582cbea8a9c059984298
mayankDhiman <mdhiman536@gmail.com>
2019-05-02 00:55:47 +0530

    simulEqn solver working for all variables

--- visma/solvers/simulEqn.py
+++ visma/solvers/simulEqn.py
@@ -4,9 +4,10 @@ from visma.io.parser import tokensToString
 from visma.functions.constant import Constant
 from visma.functions.variable import Variable
 from visma.matrix.special import cramerMatrices
+from visma.io.checks import getVariables
 
 
-def coeffCalculator(LandR_tokens):
+def coeffCalculator(LandR_tokens, variables):
     '''Finds coefficients of x, y and z and the constant term when solving for 3 simulaneous equations.
 
     Arguments:
@@ -27,11 +28,11 @@ def coeffCalculator(LandR_tokens):
             )
         for _, token in enumerate(lTokens):
             if isinstance(token, Variable):
-                if token.value == ['x']:
+                if token.value == [variables[0]]:
                     coefficients[i][0] = token.coefficient
-                elif token.value == ['y']:
+                elif token.value == [variables[1]]:
                     coefficients[i][1] = token.coefficient
-                elif token.value == ['z']:
+                elif token.value == [variables[2]]:
                     coefficients[i][2] = token.coefficient
             if isinstance(token, Constant):
                 coefficients[i][3] = token.value
@@ -39,8 +40,22 @@ def coeffCalculator(LandR_tokens):
     return coefficients
 
 
-def getResult(matD, matDx, matDy, matDz):
-    '''Calculates values of x, y and z'''
+def getResult(matD, matDx, matDy, matDz, variables, comments, animation, solveFor):
+    '''Calculates values of x, y and z
+
+    Arguments:
+    matD {visma.matrix.structure.Matrix.SquareMat} -- Matrix Token
+    matD {visma.matrix.structure.Matrix.SquareMat} -- Matrix Token
+    matD {visma.matrix.structure.Matrix.SquareMat} -- Matrix Token
+    matD {visma.matrix.structure.Matrix.SquareMat} -- Matrix Token
+    comments {list} -- list of comments
+    animation {list} -- equation tokens for step by step
+
+    Returns:
+    comments {list} -- list of comments
+    animations {list} -- list of step by step tokens
+    trivial {bool} -- Indicates if trivial solutions exist or not
+    '''
     x = 0
     y = 0
     z = 0
@@ -49,45 +64,88 @@ def getResult(matD, matDx, matDy, matDz):
     detDx = float(tokensToString(matDx.determinant()))
     detDy = float(tokensToString(matDy.determinant()))
     detDz = float(tokensToString(matDz.determinant()))
+    comments += [['Determinant value of first Crammer Matrix D = ' + str(detD)]]
+    animation += [[]]
+    comments += [['Determinant value of second Cramer Matrix Dx = ' + str(detDx)]]
+    animation += [[]]
+    comments += [['Determinant value of third Cramer Matrix Dy = ' + str(detDy)]]
+    animation += [[]]
+    comments += [['Determinant value of fourth Cramer Matrix Dz = ' + str(detDz)]]
+    animation += [[]]
     if detD == 0:
         trivial = False
     if trivial:
         x = detDx/detD
         y = detDy/detD
         z = detDz/detD
-    return trivial, x, y, z
+    resultStr = ''
+    if trivial:
+        if solveFor == variables[0]:
+            resultStr = str(variables[0]) + ' = ' + str(x)
+            comments += [[]]
+            animation += [tokenizer(resultStr)]
+        elif solveFor == variables[1]:
+            resultStr = str(variables[1]) + ' = ' + str(y)
+            comments += [[]]
+            animation += [tokenizer(resultStr)]
+        elif solveFor == variables[2]:
+            resultStr = str(variables[2]) + ' = ' + str(z)
+            comments += [[]]
+            animation += [tokenizer(resultStr)]
+    elif not trivial:
+        comments += [['There is no trivial solution to the the provided set of equations as D = 0']]
+        animation += [[]]
+    return comments, animation, trivial
 
 
-def simulSolver(eqStr1, eqStr2, eqStr3, solveFor):
+def simulSolver(eqTok1, eqTok2, eqTok3, solveFor):
     '''
     Main driver function in simulEqn.py
 
     Arguments:
-    eqStr{i} -- i = 1, 2, 3 -- equation strings of equations inputted by user.
+    eqTok1 {lsit} -- list of tokens of first equation
+    eqTok2 {lsit} -- list of tokens of second equation
+    eqTok3 {lsit} -- list of tokens of third equation
     solveFor -- string/character -- variable for which equation is being solved.
 
     Returns:
-    outputString -- Either value of variable to be solved for OR
-                    Prompt indicating non trivial solution
+    tokenLastString {string} -- last step in string form
+    animations {list} -- list of step by step tokens
+    comments {list} -- list of comments
     '''
-    eqnStrs = [eqStr1, eqStr2, eqStr3]   # Stores list of strings for 3 equations.
-    eqnTokens = [tokenizer(eqn) for eqn in eqnStrs]   # Stores lisk of tokens for each equation.
+    animation = []
+    comments = []
+
+    variables = []   # List of variables with respect to which equation can be solved.
+    eqnTokens = [eqTok1, eqTok2, eqTok3]   # Stores list of strings for 3 equations.
     LandR_tokens = []   # ith entry stores L & R tokens for ith eqations.
+
     for _, tokens in enumerate(eqnTokens):
         lTokens, rTokens = getLHSandRHS(tokens)
+        variablesEach = getVariables(lTokens, rTokens)
+        variables.extend(variablesEach)
         LandR_tokens.append([lTokens, rTokens])
+    variables = list(dict.fromkeys(variables))
+    variables.sort()    # List of all the variables in all 3 equation in sorted order
+
     for i, tokens in enumerate(LandR_tokens):
-        lTokens, rTokens, _, _, _, _ = simplifyEquation(tokens[0], tokens[1])
+        lTokens, rTokens, _, _, animationEach, commentsEach = simplifyEquation(tokens[0], tokens[1])
+        animationEach = [[]] + animationEach
+        if i == 0:
+            commentsEach = [['Simplifying the ' + str(i + 1) + 'st ' + 'equation']] + commentsEach
+        if i == 1:
+            commentsEach = [['Simplifying the ' + str(i + 1) + 'nd ' + 'equation']] + commentsEach
+        elif i == 2:
+            commentsEach = [['Simplifying the ' + str(i + 1) + 'rd  ' + 'equation']] + commentsEach
+        animation.extend(animationEach)
+        comments.extend(commentsEach)
         LandR_tokens[i] = [lTokens, rTokens]
-    coefficients = coeffCalculator(LandR_tokens)   # (as of now) its a 3 by 4 matrix; each row has coeff of x, y, z and constant term.
+
+    coefficients = coeffCalculator(LandR_tokens, variables)   # (as of now) its a 3 by 4 matrix; each row has coeff of x, y, z and constant term.
     matD, matDx, matDy, matDz = cramerMatrices(coefficients)
-    trivial, x, y, z = getResult(matD, matDx, matDy, matDz)
+    comments, animation, trivial = getResult(matD, matDx, matDy, matDz, variables, comments, animation, solveFor)
     if trivial:
-        if solveFor == 'x':
-            return ('x = ' + str(x))
-        elif solveFor == 'y':
-            return ('y = ' + str(y))
-        elif solveFor == 'z':
-            return ('z = ' + str(z))
+        lastTokenString = tokensToString(animation[-1])
     else:
-        return 'No Trivial Solution'
+        lastTokenString = 'No Trivial Solution'
+    return lastTokenString, animation, comments
