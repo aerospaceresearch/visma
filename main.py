d8516be5d0235941dbc01ac98ea0b6e67d221fa9
ZouvikPan <souvik.pan2001@gmail.com>
2020-02-25 03:06:01 +0530

    Corrected typos in main.py

--- main.py
+++ main.py
@@ -15,26 +15,26 @@ def init():
         '''This inititates the main VisMa Prompt from where user may move to CLI/GUI'''
 
         userManual = "|_________________________________________________________________________________________________|\n"\
-                     "| gui  ->> opens Visma in GUI mode.                                                               |\n"\
-                     "| Ctrl + D ->> Closes the prompt.                                                                     |\n"\
+                     "| gui ->> Opens Visma in GUI mode.                                                                |\n"\
+                     "| Ctrl + D ->> Closes the prompt.                                                                 |\n"\
                      "| exit ->> Closes the prompt.                                                                     |\n"\
                      "|-------------------------------------------------------------------------------------------------|\n"\
                      "| simplify( equation or expression ) ->> Simplifies the given equation.                           |\n"\
                      "| addition( equation or expression ) ->> Adds the elements used.                                  |\n"\
                      "| subtraction( equation or expression ) ->> Subtracts the elements used.                          |\n"\
-                     "| multiplication( equation or expression )  ->> Multiplies the elements used.                     |\n"\
-                     "| division( equation or expression )  ->> Divides the elements used.                              |\n"\
+                     "| multiplication( equation or expression ) ->> Multiplies the elements used.                      |\n"\
+                     "| division( equation or expression ) ->> Divides the elements used.                               |\n"\
                      "|-------------------------------------------------------------------------------------------------|\n"\
-                     "| factorize( expression )  ->> Factorizes the expression.                                         |\n"\
-                     "| find-roots( equation )  ->> Solves the quadratic equation for the variable in the equation.     |\n"\
-                     "| solve( equation , variable )  ->> Solves the equation for the given variable.                   |\n"\
+                     "| factorize( expression ) ->> Factorizes the expression.                                          |\n"\
+                     "| find-roots( equation ) ->> Solves the quadratic equation for the variable in the equation.      |\n"\
+                     "| solve( equation , variable ) ->> Solves the equation for the given variable.                    |\n"\
                      "|-------------------------------------------------------------------------------------------------|\n"\
-                     "| integrate( expression , variable )  ->> Integrates the expression by the given variable.        |\n"\
+                     "| integrate( expression , variable ) ->> Integrates the expression by the given variable.         |\n"\
                      "| differentiate( expression , variable ) ->> Differentiates the expression by the given variable. |\n"\
                      "|_________________________________________________________________________________________________|\n"\
 
         prompt = '>>> '
-        intro = "Welcome! This is Visual Maths Interactive Shell...\n" + "type 'help' for a User Manual and Ctrl + D to Exit prompt\n"
+        intro = "Welcome! This is Visual Maths Interactive Shell...\n" + "Type 'help' for a User Manual.\n" + "Press Ctrl + D or type 'exit' to Exit prompt.\n"
 
         def do_exit(self, inp):
             '''Exits VisMa Prompt'''
