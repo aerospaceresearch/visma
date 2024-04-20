a2e9ccb1dba7ab74c142b72aa984aa20465e6576
Daniel Acosta <danielx1611@gmail.com>
2024-04-20 17:43:06 -0400

    Fixed variable parsing error with rounding step by step solution

--- visma/io/parser.py
+++ visma/io/parser.py
@@ -70,15 +70,25 @@ def resultLatex(equationTokens, operation, comments, solutionType, simul=False, 
 
 def round_equation_latex_output(equationLatex, index, round_length):
     equationSlice = equationLatex[index][0:]
+    print(equationSlice)
     while '{' in equationSlice:
         open_bracket_index = equationSlice.index("{")
         close_bracket_index = equationSlice.index("}")
-        value = round(float(equationSlice[open_bracket_index + 1:close_bracket_index]), round_length)
-        print(equationLatex[index])
-        equationLatex[index] = equationLatex[index][0:equationLatex[index].index(equationSlice)] \
-                                + equationSlice[0: open_bracket_index] + '{' + str(value) + '}' + equationSlice[close_bracket_index + 1:]
-        print(equationLatex[index])
-        equationSlice = equationSlice[close_bracket_index + 1:]
+        if not equationSlice[close_bracket_index - 1].isnumeric():
+            value = round(float(equationSlice[:open_bracket_index]), round_length)
+            print(equationLatex[index])
+            equationLatex[index] = equationLatex[index][0:equationLatex[index].index(equationSlice)] \
+                                    + str(value) + equationSlice[open_bracket_index:]
+            print(equationLatex[index])
+            equationSlice = equationSlice[close_bracket_index + 1:]
+            pass
+        else:
+            value = round(float(equationSlice[open_bracket_index + 1:close_bracket_index]), round_length)
+            print(equationLatex[index])
+            equationLatex[index] = equationLatex[index][0:equationLatex[index].index(equationSlice)] \
+                                    + equationSlice[0: open_bracket_index] + '{' + str(value) + '}' + equationSlice[close_bracket_index + 1:]
+            print(equationLatex[index])
+            equationSlice = equationSlice[close_bracket_index + 1:]
     return equationLatex[index]
 
 def resultStringCLI(equationTokens, operation, comments, solutionType, simul=False, mat=False):
