99cfebd17ddefc1196481a6f6dc8e9a5380b25cb
AceShooter <nazariowork1@gmail.com>
2024-04-21 21:15:27 -0400

    Refactoring shennanigans

--- visma/functions/structure.py
+++ visma/functions/structure.py
@@ -11,92 +11,92 @@ def tokensToString(tokens):
         tokenString {string} -- equation string
     """
     # FIXME: tokensToString method
-    tokenString = ''
-    for token in tokens:
-        if isinstance(token, Constant):
-            if isinstance(token.value, list):
-                for j, val in token.value:
-                    if token['power'][j] != 1:
-                        tokenString += (str(val) + '^(' + str(token.power[j]) + ')')
-                    else:
-                        tokenString += str(val)
-            elif isNumber(token.value):
-                if token.power != 1:
-                    tokenString += (str(token.value) + '^(' + str(token.power) + ')')
-                else:
-                    tokenString += str(token.value)
-        elif isinstance(token, Variable):
-            if token.coefficient == 1:
-                pass
-            elif token.coefficient == -1:
-                tokenString += '-'
-            else:
-                tokenString += str(token.coefficient)
-            for j, val in enumerate(token.value):
-                if token.power[j] != 1:
-                    tokenString += (str(val) + '^(' + str(token.power[j]) + ')')
-                else:
-                    tokenString += str(val)
-        elif isinstance(token, Binary):
-            tokenString += ' ' + str(token.value) + ' '
-        elif isinstance(token, Expression):
-            if token.coefficient != 1:
-                tokenString += str(token.coefficient) + '*'
-            tokenString += '('
-            tokenString += tokensToString(token.tokens)
-            tokenString += ')'
-            if token.power != 1:
-                tokenString += '^(' + str(token.power) + ')'
-        elif isinstance(token, Sqrt):
-            tokenString += 'sqrt['
-            if isinstance(token.power, Constant):
-                tokenString += tokensToString([token.power])
-            elif isinstance(token.power, Variable):
-                tokenString += tokensToString([token.power])
-            elif isinstance(token.power, Expression):
-                tokenString += tokensToString(token.power.tokens)
-            tokenString += ']('
-            if isinstance(token.operand, Constant):
-                tokenString += tokensToString([token.operand])
-            elif isinstance(token.operand, Variable):
-                tokenString += tokensToString([token.operand])
-            elif isinstance(token.operand, Expression):
-                tokenString += tokensToString(token.operand.tokens)
-            tokenString += ')'
-        elif isinstance(token, Logarithm):
-            if token.coefficient == 1:
-                pass
-            elif token.coefficient == -1:
-                tokenString += '-'
-            else:
-                tokenString += str(token.coefficient)
-            if token.operand is not None:
-                tokenString += token.value
-                if token.power != 1:
-                    tokenString += "^" + "(" + str(token.power) + ")"
-                tokenString += "(" + tokensToString([token.operand]) + ")"
-        elif isinstance(token, Trigonometric):
-            if token.coefficient == 1:
-                pass
-            elif token.coefficient == -1:
-                tokenString += '-'
-            else:
-                tokenString += str(token.coefficient)
-            if token.operand is not None:
-                tokenString += token.value
-                if token.power != 1:
-                    tokenString += "^" + "(" + str(token.power) + ")"
-                tokenString += "(" + tokensToString([token.operand]) + ")"
-        elif isinstance(token, Matrix):
-            tokenString += "["
-            for i in range(token.dim[0]):
-                for j in range(token.dim[1]):
-                    tokenString += tokensToString(token.value[i][j])
-                    tokenString += ","
-                tokenString = tokenString[:-1] + ";"
-            tokenString = tokenString[:-1] + "]"
-
-    return tokenString
+    # tokenString = ''
+    # for token in tokens:
+    #     if isinstance(token, Constant):
+    #         if isinstance(token.value, list):
+    #             for j, val in token.value:
+    #                 if token['power'][j] != 1:
+    #                     tokenString += (str(val) + '^(' + str(token.power[j]) + ')')
+    #                 else:
+    #                     tokenString += str(val)
+    #         elif isNumber(token.value):
+    #             if token.power != 1:
+    #                 tokenString += (str(token.value) + '^(' + str(token.power) + ')')
+    #             else:
+    #                 tokenString += str(token.value)
+    #     elif isinstance(token, Variable):
+    #         if token.coefficient == 1:
+    #             pass
+    #         elif token.coefficient == -1:
+    #             tokenString += '-'
+    #         else:
+    #             tokenString += str(token.coefficient)
+    #         for j, val in enumerate(token.value):
+    #             if token.power[j] != 1:
+    #                 tokenString += (str(val) + '^(' + str(token.power[j]) + ')')
+    #             else:
+    #                 tokenString += str(val)
+    #     elif isinstance(token, Binary):
+    #         tokenString += ' ' + str(token.value) + ' '
+    #     elif isinstance(token, Expression):
+    #         if token.coefficient != 1:
+    #             tokenString += str(token.coefficient) + '*'
+    #         tokenString += '('
+    #         tokenString += tokensToString(token.tokens)
+    #         tokenString += ')'
+    #         if token.power != 1:
+    #             tokenString += '^(' + str(token.power) + ')'
+    #     elif isinstance(token, Sqrt):
+    #         tokenString += 'sqrt['
+    #         if isinstance(token.power, Constant):
+    #             tokenString += tokensToString([token.power])
+    #         elif isinstance(token.power, Variable):
+    #             tokenString += tokensToString([token.power])
+    #         elif isinstance(token.power, Expression):
+    #             tokenString += tokensToString(token.power.tokens)
+    #         tokenString += ']('
+    #         if isinstance(token.operand, Constant):
+    #             tokenString += tokensToString([token.operand])
+    #         elif isinstance(token.operand, Variable):
+    #             tokenString += tokensToString([token.operand])
+    #         elif isinstance(token.operand, Expression):
+    #             tokenString += tokensToString(token.operand.tokens)
+    #         tokenString += ')'
+    #     elif isinstance(token, Logarithm):
+    #         if token.coefficient == 1:
+    #             pass
+    #         elif token.coefficient == -1:
+    #             tokenString += '-'
+    #         else:
+    #             tokenString += str(token.coefficient)
+    #         if token.operand is not None:
+    #             tokenString += token.value
+    #             if token.power != 1:
+    #                 tokenString += "^" + "(" + str(token.power) + ")"
+    #             tokenString += "(" + tokensToString([token.operand]) + ")"
+    #     elif isinstance(token, Trigonometric):
+    #         if token.coefficient == 1:
+    #             pass
+    #         elif token.coefficient == -1:
+    #             tokenString += '-'
+    #         else:
+    #             tokenString += str(token.coefficient)
+    #         if token.operand is not None:
+    #             tokenString += token.value
+    #             if token.power != 1:
+    #                 tokenString += "^" + "(" + str(token.power) + ")"
+    #             tokenString += "(" + tokensToString([token.operand]) + ")"
+    #     elif isinstance(token, Matrix):
+    #         tokenString += "["
+    #         for i in range(token.dim[0]):
+    #             for j in range(token.dim[1]):
+    #                 tokenString += tokensToString(token.value[i][j])
+    #                 tokenString += ","
+    #             tokenString = tokenString[:-1] + ";"
+    #         tokenString = tokenString[:-1] + "]"
+    #
+    # return tokenString
 
 
 class Function(object):
